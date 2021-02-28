f6a8a0af16615b2b55f1475f7fa9a22fe5272e97
Bellriox <autijw@gmail.com>
2021-02-28 13:28:47 +0800

    Filled in infos about myself

--- js/reveal.js
+++ js/reveal.js
@@ -1,32 +1,32 @@
 window.sr = ScrollReveal({ reset: true });
 
 // Custom Settings
-sr.reveal('.header-title', {
-    origin: 'right',
-    duration: 1700
-});
+// sr.reveal(".header-title", {
+//   origin: "right",
+//   duration: 1700,
+// });
 
-sr.reveal('.header-sub', {
-    origin: 'right',
-    duration: 1700
-});
+// sr.reveal(".header-sub", {
+//   origin: "right",
+//   duration: 1700,
+// });
 
-sr.reveal('.projects-reveal', {
-    origin: 'right',
-    duration: 1700
-});
+// // sr.reveal('.projects-reveal', {
+// //     origin: 'right',
+// //     duration: 1700
+// // });
 
-sr.reveal('.row', {
-    origin: 'right',
-    duration: 1700
-});
+// sr.reveal(".row", {
+//   origin: "right",
+//   duration: 1700,
+// });
 
-sr.reveal('.row-button', {
-    origin: 'left',
-    duration: 1700
-});
+// sr.reveal(".row-button", {
+//   origin: "left",
+//   duration: 1700,
+// });
 
-sr.reveal('.footer', {
-    origin: 'left',
-    duration: 1300
-});
+// sr.reveal(".footer", {
+//   origin: "left",
+//   duration: 1300,
+// });
--- js/status.js
+++ js/status.js
@@ -1,81 +1,90 @@
 function loadDay() {
-    const num = new Date().getDay();
-    switch (num) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-    }
-    return day
+  const num = new Date().getDay();
+  switch (num) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+  return day;
 }
 
 function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    var day = loadDay()
-    document.getElementById('time-text').innerHTML =
-        day + " " + h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  var day = loadDay();
+  document.getElementById("time-text").innerHTML =
+    day + " " + h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
 }
 
 function checkTime(i) {
-    if (i < 10) { i = "0" + i }; // add zero in front of numbers < 10
-    return i;
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
 }
 
 function loadStatus() {
-    startTime()
-    loadAvailability()
-    loadLastUpdated()
+  startTime();
+  loadAvailability();
+  loadLastUpdated();
 }
 
 function loadLastUpdated() {
-    var first = 9
-    var second = Math.floor(Math.random() * 10)
-    var third = Math.floor(Math.random() * 10)
-    var final = first + '.' + second + '.' + third
-    document.getElementById('update').innerHTML = "Last Updated " + final
+  var first = 9;
+  var second = Math.floor(Math.random() * 10);
+  var third = Math.floor(Math.random() * 10);
+  var final = first + "." + second + "." + third;
 }
 
 function loadAvailability() {
-    var now = new Date();
-    var dayOfWeek = now.getDay();
-    var hour = now.getHours()
-    var minutes = now.getMinutes()
+  var now = new Date();
+  var dayOfWeek = now.getDay();
+  var hour = now.getHours();
+  var minutes = now.getMinutes();
 
-    if (dayOfWeek > 0 && dayOfWeek < 6) {
-        if (hour > 9 && (hour < 15 && minutes > 0)) {
-            document.getElementById('availability').innerHTML = "Unavailable"
-            document.getElementById('small-rectangle').style.backgroundColor = "red"
-            document.getElementById('status-text').innerHTML = `<i class="fas fa-school"></i> In School`
-        } else {
-            document.getElementById('availability').innerHTML = "Available"
-            document.getElementById('small-rectangle').style.backgroundColor = "limegreen"
-            document.getElementById('status-text').innerHTML = `<i class="fas fa-signal"></i> Online`
-        }
+  if (dayOfWeek > 0 && dayOfWeek < 6) {
+    if (hour > 9 && hour < 15 && minutes > 0) {
+      document.getElementById("availability").innerHTML = "Unavailable";
+      document.getElementById("small-rectangle").style.backgroundColor = "red";
+      document.getElementById(
+        "status-text"
+      ).innerHTML = `<i class="fas fa-school"></i> In School`;
     } else {
-        document.getElementById('availability').innerHTML = "Available"
-        document.getElementById('small-rectangle').style.backgroundColor = "limegreen"
-        document.getElementById('status-text').innerHTML = `<i class="fas fa-signal"></i> Online`
+      document.getElementById("availability").innerHTML = "Available";
+      document.getElementById("small-rectangle").style.backgroundColor =
+        "limegreen";
+      document.getElementById(
+        "status-text"
+      ).innerHTML = `<i class="fas fa-signal"></i> Online`;
     }
+  } else {
+    document.getElementById("availability").innerHTML = "Available";
+    document.getElementById("small-rectangle").style.backgroundColor =
+      "limegreen";
+    document.getElementById(
+      "status-text"
+    ).innerHTML = `<i class="fas fa-signal"></i> Online`;
+  }
 }
